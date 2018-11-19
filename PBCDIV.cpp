61b0b0dc604c3f61b043b255413b044e51d95283
thanhlvcpy <42794074+thanhlvcpy@users.noreply.github.com>
2018-11-19 16:14:09 +0700

    Update PBCDIV.cpp

--- PBCDIV.cpp
+++ PBCDIV.cpp
@@ -1,19 +1,16 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 int n;
 int main(){
 	cin>>n;
-	int a,b;
+	long long a,b;
 	for(int i=1;i<=n;i++){
-		scanf("%lld%lld",&a,&b);	//cin
-		int dem1,dem2,dem3;
+		cin>>a>>b;
+        long long dem1,dem2,dem3;
 		dem1 = b/12-(a-1)/12;
 		dem2 = b/30-(a-1)/30;
 		dem3 = b/60-(a-1)/60;
-		printf("%lld\n",dem1+dem2-2*dem3);	//cout
+		cout<<dem1+dem2-2*dem3<<endl;
 	}
 	return 0;
 }
-//scanf("%lld%lld",&a,&b);
-//printf("%lld\n",calc(b)-calc(a-1));
